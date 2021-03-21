ca32bd424679c1c167768cacd54e3a291a4e6e5c
thoorin <marek.skreko@gmail.com>
2021-03-21 16:20:36 +0100

    reformatting

--- game.lua
+++ game.lua
@@ -28,8 +28,12 @@ M.resetVariables = function()
     canJump, ply, shoots, stopped, game = false, nil, false, false, true
 end
 
-M.setUpGame = function( comp,c,bA,i)
+M.setUp = function( comp,c,bA,i)
     composer, creator, blocksArray, interface = comp, c, bA, i
+end
+
+M.setBlocks = function( bA )
+    blocksArray = bA
 end
 
 M.setJump = function( boolean )
